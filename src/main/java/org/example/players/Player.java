f21c4fe6e9c4b691ecff86793db694cc0db8907d
PaterSSS <leo.chel@mail.ru>
2023-12-20 20:45:02 +0300

    template for player

--- src/main/java/org/example/players/Player.java
+++ src/main/java/org/example/players/Player.java
@@ -1,4 +1,20 @@
 package org.example.players;
 
+import org.example.units.ProtoShip;
+
+import java.util.List;
+
 public interface Player {
+    enum PossibleMoves {
+        MOVE_RIGHT,
+        MOVE_LEFT,
+        MOVE_UP,
+        MOVE_DOWN,
+        SHOOT,
+        CLOCKWISE_ROTATE,
+        COUNTERCLOCKWISE_ROTATE
+    }
+    List<ProtoShip> getAliveShips();
+    void shipIsEliminated(ProtoShip ship);
+    PossibleMoves makeMove();
 }
